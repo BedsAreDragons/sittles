6c721bb6418e3902aefd9ead8322a3e62b4a4b2d
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-04-09 14:31:58 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,23 @@
 from flask import Flask, render_template
 import os
+import requests
 
 app = Flask(__name__)
 
+def get_metar():
+    url = "https://api.checkwx.com/station/EGBB"
+    headers = {"X-API-Key": "d67885927a1247dbaf838b7065"}
+    response = requests.get(url, headers=headers)
+    if response.status_code == 200:
+        data = response.json()
+        return data['data']['0']['raw_text']
+    else:
+        return "Failed to fetch METAR data"
+
 @app.route('/')
 def index():
-    return render_template('index.html')
+    metar = get_metar()
+    return render_template('index.html', metar=metar)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("WEB_PORT", 5000, )))
+    app.run(host='0.0.0.0', port=int(os.getenv("WEB_PORT", 5000)))
