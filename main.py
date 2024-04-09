96ef7d084608b672a937403bb778b63635c45011
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-04-09 14:41:42 +0100

    Update main.py

--- main.py
+++ main.py
@@ -20,4 +20,4 @@ def index():
     return render_template('index.html', metar=metar)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=int(os.getenv("WEB_PORT", 5000)))
+    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("WEB_PORT", 5000)))
