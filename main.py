ccbf4cf2af07fe10f7d99a3492851b2716ecc4bc
BedsAreDragons <85127998+BedsAreDragons@users.noreply.github.com>
2024-04-10 11:36:21 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 from flask import Flask, render_template
-import os
 import requests
 
 app = Flask(__name__)
 
 def get_metar():
-    url = "https://api.checkwx.com/station/EGBB"
-    headers = {"X-API-Key": "d67885927a1247dbaf838b7065"}
-    response = requests.get(url, headers=headers)
+    url = "https://avwx.rest/api/metar/EGBB"
+    headers = {"Authorization": "Token my_secret_api_token"}
+    params = {
+        "format": "json"
+    }
+    response = requests.get(url, headers=headers, params=params)
     if response.status_code == 200:
         data = response.json()
-        print(data)  # Add this line to inspect the structure of the response
-        return data
+        return data['sanitized']  # 'sanitized' key contains the METAR information
     else:
         return "Failed to fetch METAR data"
-
-
-
 
 @app.route('/')
 def index():
     metar = get_metar()
-    print(metar)
     return render_template('index.html', metar=metar)
 
 if __name__ == '__main__':
-    
-    app.run(debug=True, host='0.0.0.0', port=int(os.getenv("WEB_PORT", 5000)))
+    app.run(host='0.0.0.0', port=5000)
